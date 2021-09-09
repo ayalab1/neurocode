ed9f1262111eb3863a3faf722445651f1aaa7308
Ryan Harvey <ryanh412@gmail.com>
2021-09-09 12:21:33 -0400

    < 50 spikes is unreliable for distribution estimation so default to nan

--- spikeLFP/phaseModulation.m
+++ spikeLFP/phaseModulation.m
@@ -167,7 +167,7 @@ for a = 1:length(spikes.times)
     bools = InIntervals(spikes.times{a},intervals);
     s =spikes.times{a}(bools);
     %     s = spikes{a};
-    if isempty(s)
+    if isempty(s) || length(s) < 50 
         phasedistros(:,a) = zeros(numBins,1);
         phasestats.m(a) = nan;
         phasestats.r(a) = nan;

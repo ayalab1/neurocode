d0a1d54410d9672efa94241698899068ea1a76e2
raltodo <93602613+raltodo@users.noreply.github.com>
2022-02-09 12:07:49 -0500

    Set NoiseRemoval to "false" by default as it doesn't work
    NoiseRemoval uses a unidirectional filter which shifts the data, so it removes intervals ~20ms after the supposed "noise" it detected (which is shorter than 20ms). I am also not convinced that peaks in high gamma (I got peak correlation with 70 Hz wavelet power) are necessarily noise to be removed, so I recommend this option not be used at all.
    
    The whitening step in Kilosort should already be good enough to ignore shared noise between channels.

--- preProcessing/preprocessSession.m
+++ preProcessing/preprocessSession.m
@@ -45,7 +45,7 @@ addParameter(p,'cleanArtifacts',false,@islogical);
 addParameter(p,'stateScore',false,@islogical);
 addParameter(p,'spikeSort',true,@islogical);
 addParameter(p,'getPos',true,@islogical);
-addParameter(p,'removeNoise',true,@islogical);
+addParameter(p,'removeNoise',false,@islogical); % raly: noise removal is bad, it removes periods 20ms after (because of the filter shifting) a peak in high gamma. See ayadata1\home\raly\Documents\notes\script_NoiseRemoval_bad.m for details.
 addParameter(p,'runSummary',false,@islogical);
 addParameter(p,'SSD_path','D:\KiloSort',@ischar)    % Path to SSD disk. Make it empty to disable SSD
 

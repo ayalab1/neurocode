6fdd644c41741fc15d3eab94e1ed8959f5fda8a5
Ryan Harvey <ryanh412@gmail.com>
2022-02-23 15:28:38 -0500

    added dlc bat files

---
+++ behavior/dlc/Run_analyzeYmaze.bat
@@ -0,0 +1,5 @@
+@echo on
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+call conda activate C:\Users\Schaf\anaconda3\envs\DEEPLABCUT
+python "C:\Users\Schaf\Documents\DLC Networks\scripts\analyze_ymaze.py"
+

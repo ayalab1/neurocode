9db5e6dc3c2da9b836c25709aaba137001441c0c
raltodo <93602613+raltodo@users.noreply.github.com>
2022-03-29 17:29:21 -0400

    Restoring the GNU GPL license notice. Removing this is a GPL violation.
    All of the FMAT functions include the GNU GPL notice. Basically, a GPL license is a "copyleft" license, which enforces libre access to the work and all derivatives. You can freely use and modify the code *as long as your releases remain free software*. Code shared on github without any license is not really free software.
    
    Since we are distributing the code (as it's readily available in our public github), every time you modify and build on code that is under the GNU GPL licence, you need to have your version (if you're distributing it) under that license as well. This prevents the situation where someone shares the code with some company (e.g. by uploading it to FileExchange on Mathworks, which does not allow such licenses) that then decides to copyright it from themselves (e.g. use it in a future toolbox) and suing people who try to build on it in the future.
    
    I always include the GPL license in my code (except when I forget :D).

--- loadData/getLFP.m
+++ loadData/getLFP.m
@@ -54,6 +54,12 @@ function [lfp] = getLFP(varargin)
 % Copyright (C) 2004-2011 by Michaël Zugaro
 % editied by David Tingley, 2017
 % kathryn mcclain 2020
+% Ralitsa Todorova 2022
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
 %
 % NOTES
 % -'select' option has been removed, it allowed switching between 0 and 1

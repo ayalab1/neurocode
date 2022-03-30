e524aaa8ecd646a22cd39b883abc26a8cb34ecf6
Heath Robinson <heathlarssonrobinson@gmail.com>
2022-03-30 12:21:39 -0400

    added multi training to objPlaceScore via detection of sessionSeq
    baseline sessionSeq still works for muli training trainings 1,2,3 and test 4

--- behavior/objPlaceScore.m
+++ behavior/objPlaceScore.m
@@ -8,11 +8,13 @@ function [objScore] = objPlaceScore(sessionsSeq,basepath,fs,timeTh)
 %         1 = training
 %         2 = test
 %    default: [0 0 1 0 2] 
+%    for multi-training(3 training session) make training 1,2,3 and test 4
 % fs default: 30000
 % timeTh: [time training time test] to include in the analyis in seconds (discard time after it)
 %    default: [300 300]
 % 
 % AntonioFR, 2020
+% added multi training - HeathLarsson 03/2022
 
 if ~exist('basepath','var')
     basepath = pwd;
@@ -26,7 +28,7 @@ end
 if ~exist('timeTh','var')
     timeTh = [600 600];
 end  
-
+  
 %% order individual ses
 cd(basepath);
 %basename = bz_BasenameFromBasepath(basepath);
@@ -60,36 +62,101 @@ end
 
 %% get scores from each session
 % objA=control; objB=displaced
+% added multi training
+% could/should be improved to detect multi/single HeathLarsson 03/2022\
+
+sum_sessionSeq=sum(sessionsSeq);
+
+if sum_sessionSeq>3
+    
 cd([basepath '\' nsespaths{find(sessionsSeq == 1)}]);
-data_training = getDigitalIn('all','fs', fs, 'offset', 0, 'periodLag', 1);
+data_training_1 = getDigitalIn('all','fs', fs, 'offset', 0, 'periodLag', 1);
 cd([basepath '\' nsespaths{find(sessionsSeq == 2)}]);
-data_test = getDigitalIn('all','fs', fs, 'offset', 0, 'periodLag', 1);
+data_training_2 = getDigitalIn('all','fs', fs, 'offset', 0, 'periodLag', 1);
+cd([basepath '\' nsespaths{find(sessionsSeq == 3)}]);
+data_training_3 = getDigitalIn('all','fs', fs, 'offset', 0, 'periodLag', 1);
+cd([basepath '\' nsespaths{find(sessionsSeq == 4)}]);
+data_test_multi = getDigitalIn('all','fs', fs, 'offset', 0, 'periodLag', 1);
 
 cd(basepath);
 
 %
+
 if isempty(timeTh)
-    object(1,:) = sum(data_training.dur{1,2}); % time in seconds spent with interacting object A during training
-    object(2,:) = sum(data_training.dur{1,3}); % time in seconds spent with interacting object B during training
-    object(3,:) = sum(data_test.dur{1,2}); %time in seconds spent with interacting object A during test
-    object(4,:) = sum(data_test.dur{1,3}); %time in seconds spent with interacting object B during test
+    object_training_1(1,:) = sum(data_training_1.dur{1,2}); % time in seconds spent with interacting object A during training
+    object_training_1(2,:) = sum(data_training_1.dur{1,3}); % time in seconds spent with interacting object B during training
+    object_training_2(1,:) = sum(data_training_2.dur{1,2}); % time in seconds spent with interacting object A during training
+    object_training_2(2,:) = sum(data_training_2.dur{1,3}); % time in seconds spent with interacting object B during training
+    object_training_3(1,:) = sum(data_training_3.dur{1,2}); % time in seconds spent with interacting object A during training
+    object_training_3(2,:) = sum(data_training_3.dur{1,3}); % time in seconds spent with interacting object B during training
+    
+    object_test_multi(1,:) = sum(data_test_multi.dur{1,2}); %time in seconds spent with interacting object A during test
+    object_test_multi(2,:) = sum(data_test_multi.dur{1,3}); %time in seconds spent with interacting object B during test
+else
+    times_train1_1 = find(data_training_1.timestampsOn{2}<timeTh(1));
+    times_train1_2 = find(data_training_1.timestampsOn{3}<timeTh(1));
+    object_training_1(1,:) = sum(data_training_1.dur{1,2}(times_train1_1)); 
+    object_training_1(2,:) = sum(data_training_1.dur{1,3}(times_train1_2)); 
+    times_train2_1 = find(data_training_2.timestampsOn{2}<timeTh(1));
+    times_train2_2 = find(data_training_2.timestampsOn{3}<timeTh(1));
+    object_training_2(1,:) = sum(data_training_2.dur{1,2}(times_train2_1)); 
+    object_training_2(2,:) = sum(data_training_2.dur{1,3}(times_train2_2));
+    times_train3_1 = find(data_training_3.timestampsOn{2}<timeTh(1));
+    times_train3_2 = find(data_training_3.timestampsOn{3}<timeTh(1));
+    object_training_3(1,:) = sum(data_training_3.dur{1,2}(times_train3_1)); 
+    object_training_3(2,:) = sum(data_training_3.dur{1,3}(times_train3_2));
+    
+    
+    times_test = find(data_test_multi.timestampsOn{2}<timeTh(2));
+    times_test = find(data_test_multi.timestampsOn{3}<timeTh(2));
+    object_test_multi(1,:) = sum(data_test_multi.dur{1,2}(times_test)); 
+    object_test_multi(2,:) = sum(data_test_multi.dur{1,3}(times_test));     
+end
+
+
+object_A_training = (object_training_1(1,:) + object_training_2(1,:) + object_training_3(1,:))/3; %mean in order to not skew object preference score HLR 03/2022
+object_B_training = (object_training_1(2,:) + object_training_2(2,:) + object_training_3(2,:))/3; %mean in order to not skew object preference score HLR 03/2022
+object_A_test = object_test_multi(1,:);
+object_B_test = object_test_multi(2,:);   
+    
+
+else
+    
+    
+cd([basepath '\' nsespaths{find(sessionsSeq == 1)}]);
+data_training = getDigitalIn('all','fs', fs, 'offset', 0, 'periodLag', 1);
+cd([basepath '\' nsespaths{find(sessionsSeq == 2)}]);
+data_test_single = getDigitalIn('all','fs', fs, 'offset', 0, 'periodLag', 1);
+
+cd(basepath);
+
+%
+
+if isempty(timeTh)
+    object_train(1,:) = sum(data_training.dur{1,2}); % time in seconds spent with interacting object A during training
+    object_train(2,:) = sum(data_training.dur{1,3}); % time in seconds spent with interacting object B during training
+    object_test(1,:) = sum(data_test_single.dur{1,2}); %time in seconds spent with interacting object A during test
+    object_test(2,:) = sum(data_test_single.dur{1,3}); %time in seconds spent with interacting object B during test
 else
     times1 = find(data_training.timestampsOn{2}<timeTh(1));
     times2 = find(data_training.timestampsOn{3}<timeTh(1));
-    object(1,:) = sum(data_training.dur{1,2}(times1)); 
-    object(2,:) = sum(data_training.dur{1,3}(times2)); 
+    object_train(1,:) = sum(data_training.dur{1,2}(times1)); 
+    object_train(2,:) = sum(data_training.dur{1,3}(times2)); 
     
     times3 = find(data_test.timestampsOn{2}<timeTh(2));
     times4 = find(data_test.timestampsOn{3}<timeTh(2));
-    object(3,:) = sum(data_test.dur{1,2}(times3)); 
-    object(4,:) = sum(data_test.dur{1,3}(times4));     
+    object_test(1,:) = sum(data_test_single.dur{1,2}(times3)); 
+    object_test(2,:) = sum(data_test_single.dur{1,3}(times4));     
 end
 
-object_A_training = object(1,:);
-object_B_training = object(2,:);
-object_A_test = object(3,:);
-object_B_test = object(4,:);
 
+
+object_A_training = object_train(1,:);
+object_B_training = object_train(2,:);
+object_A_test = object_test(1,:);
+object_B_test = object_test(2,:);
+
+end
 % calculating discrimination index (novel-familiar / familiar + novel *100)
 DI = (object_B_test - object_A_test) / (object_A_test + object_B_test) *100;
 % calculating normalized object preference based on Ted Abel's 2014 paper

f4a90c0c36909cdfb8fbaca7219ebf0cc60a7669
Ryan Harvey <ryanh412@gmail.com>
2022-03-31 17:54:02 -0400

    dlc auto scripts

---
+++ behavior/dlc/analyze_plus_maze.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  2 16:33:27 2021
+
+@author: Matt Isaacson
+adapted by Ryan H
+"""
+
+import os
+os.environ["DLClight"] = "False"
+import tensorflow as tf
+print(tf.__version__)
+
+from tensorflow.python.client import device_lib
+device_lib.list_local_devices()
+
+os.environ["DLClight"] = "True"
+
+import deeplabcut
+
+path_config_file = r"D:\dlc_videos\social_plus_maze_dlc\social_plus_maze-harvey-2022-03-03\config.yaml"
+
+videos_folder = [r'D:\dlc_videos\social_plus_maze_v2']
+
+import sys
+sys.path.append(r"D:\github\neurocode\preProcessing")
+import find_and_move_videos
+print('moving videos and results')
+find_and_move_videos.main(r'Y:\Praveen\SocialBehavior',r'D:\dlc_videos\social_plus_maze_v2')
+
+deeplabcut.analyze_videos(path_config_file,videos_folder,shuffle=1,save_as_csv=True)
+
+print('moving videos and results')
+find_and_move_videos.main(r'Y:\Praveen\SocialBehavior',r'D:\dlc_videos\social_plus_maze_v2')
---
+++ behavior/dlc/analyze_ymaze_implanted.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  2 16:33:27 2021
+
+@author: Matt Isaacson
+adapted by Ryan H
+"""
+
+import os
+os.environ["DLClight"] = "False"
+import tensorflow as tf
+print(tf.__version__)
+
+from tensorflow.python.client import device_lib
+device_lib.list_local_devices()
+
+os.environ["DLClight"] = "True"
+
+import deeplabcut
+
+path_config_file = r"D:\dlc_videos\v1_implanted_dlc\v1-harvey-2022-03-11\config.yaml"
+
+videos_folder = [r'D:\dlc_videos\v1_implanted']
+
+import sys
+sys.path.append(r"D:\github\neurocode\preProcessing")
+import find_and_move_videos
+print('moving videos and results')
+find_and_move_videos.main(r'Y:\V1test\AO52',r'D:\dlc_videos\v1_implanted')
+find_and_move_videos.main(r'Y:\V1test\V1Jean',r'D:\dlc_videos\v1_implanted')
+
+deeplabcut.analyze_videos(path_config_file,videos_folder,shuffle=1,save_as_csv=True)
+
+print('moving videos and results')
+find_and_move_videos.main(r'Y:\V1test\AO52',r'D:\dlc_videos\v1_implanted')
+find_and_move_videos.main(r'Y:\V1test\V1Jean',r'D:\dlc_videos\v1_implanted')

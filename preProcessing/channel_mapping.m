5d40cf8afa709357a2beb79ff82dd8864a2d5417
Ryan Harvey <ryanh412@gmail.com>
2022-04-08 16:39:38 -0400

    updated doc

--- preProcessing/channel_mapping.m
+++ preProcessing/channel_mapping.m
@@ -11,8 +11,8 @@ function channel_mapping(varargin)
 %       1. To be ran after basename.session is created to set up initial
 %       anatomical map for an animal. This will set up a unlabeled .csv.
 %       You are then to label each channel and run this function again to
-%       add that data to basename.session. Label the .csv just as you see
-%       the channels in Neuroscope. 
+%       add that data to basename.session. IMPORTANT NOTE: Label the .csv 
+%       just as you see the channels in Neuroscope Multi-column view. 
 %
 %       2. Run gui_session(session) and update brain
 %       region manually, then run this function to be consistent.

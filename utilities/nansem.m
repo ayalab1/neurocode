90e55f845eafae935f57e99a901f9fd2c5db19f6
raltodo <93602613+raltodo@users.noreply.github.com>
2022-04-12 14:33:37 -0400

    adding a 'dimension' option

--- utilities/nansem.m
+++ utilities/nansem.m
@@ -1,4 +1,4 @@
-function s = nansem(x)
+function s = nansem(x,dim)
 
 %nansem - Compute standard error of the mean (SEM), ignoring NaNs.
 %
@@ -7,8 +7,9 @@ function s = nansem(x)
 %    s = nansem(x)
 %
 %    s              vector or matrix over which the sem should be computed
-
-% Copyright (C) 2008-2011 by Michaël Zugaro
+%    dim            optionally, dimension along which to perform the analysis
+%
+% Copyright (C) 2008-2022 by Michaël Zugaro, Ralitsa Todorova
 %
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -19,11 +20,19 @@ if nargin < 1,
   error('Incorrect number of parameters (type ''help <a href="matlab:help nansem">nansem</a>'' for details).');
 end
 
-if ~isdmatrix(x) & ~isdvector(x),
-  error('Incorrect input - use vector or matrix (type ''help <a href="matlab:help nansem">nansem</a>'' for details).');
+if ~isdmatrix(x) && ~isdvector(x),
+    error('Incorrect input - use vector or matrix (type ''help <a href="matlab:help nansem">nansem</a>'' for details).');
 end
 
 if any(size(x)==1), x = x(:); end
 
-n = sum(~isnan(x));
-s = nanstd(x)./sqrt(n);
+if ~exist('dim','var')
+    n = sum(~isnan(x));
+    s = nanstd(x)./sqrt(n);
+else
+    if ~isdvector(dim,'#1')
+        error('Incorrect value for parameter ''dim'' (type ''help <a href="matlab:help nansem">nansem</a>'' for details).');
+    end
+    n = sum(~isnan(x),dim);
+    s = nanstd(x,[],dim)./sqrt(n);
+end
--- utilities/semedian.m
+++ utilities/semedian.m
@@ -1,14 +1,15 @@
-function s = semedian(x,varargin)
+function s = semedian(x,dim)
 
 %semedian - Compute standard error of the median.
 %
 %  USAGE
 %
-%    s = semedian(x)
+%    s = semedian(x,dim)
 %
 %    x              vector or matrix over which the error should be computed
-
-% Copyright (C) 2013 by Michaël Zugaro
+%    dim            optionally, dimension along which to perform the analysis
+%
+% Copyright (C) 2013-2022 by Michaël Zugaro, Ralitsa Todorova
 %
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
@@ -19,11 +20,21 @@ function s = semedian(x,varargin)
 if nargin < 1,
   error('Incorrect number of parameters (type ''help <a href="matlab:help semedian">semedian</a>'' for details).');
 end
-if ~isdmatrix(x) & ~isdvector(x),
+if ~isdmatrix(x) && ~isdvector(x),
   error('Incorrect input - use vector or matrix (type ''help <a href="matlab:help semedian">semedian</a>'' for details).');
 end
 
+if ~exist('dim','var'), dim = 1; end
+if ~isdvector(dim,'#1')
+    error('Incorrect value for parameter ''dim'' (type ''help <a href="matlab:help semedian">semedian</a>'' for details).');
+end
+
 if any(size(x)==1), x = x(:); end
+
+if exist('dim','var') && dim==2
+    s = semedian(x')';
+    return
+end
 
 n = size(x,1);
 m = repmat(nanmedian(x),n,1);

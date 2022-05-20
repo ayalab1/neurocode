bf3937dfccf43a1882a7a2001139ad347f09c350
lkaraba <karaba24@gmail.com>
2022-05-20 13:24:50 -0400

    More robust version of combining KiloSorts

--- preProcessing/PreprocessSpikes.m
+++ preProcessing/PreprocessSpikes.m
@@ -14,47 +14,58 @@ f = dir('Kilosort*');
 % Make sure there is only one KiloSort folder before running, unless you needed to spike sort probes separately
 % The hippocampal KiloSort folder should be listed first in the session folder for organization, but this is not necessary for running
 if (size(f,1) > 1) && ifMultiKiloSort
-    for i = 2:size(f,1)
+    for i = 1:size(f,1)
         session.spikeSorting{1,1}.relativePath = f(i).name;
         tempSpikes{i} = loadSpikes('session',session,'clusteringpath',[f(i).folder filesep f(i).name], 'savemat', false);
         if i == 1
             spikes = tempSpikes{i};
         else
-            spikes.ids = cat(2,spikes.ids,tempSpikes{i}.ids);
-            spikes.ts = cat(2,spikes.ts,tempSpikes{i}.ts);
-            spikes.times = cat(2, spikes.times, tempSpikes{i}.times);
-            spikes.cluID = [spikes.cluID tempSpikes{i}.cluID];
-            spikes.total = [spikes.total tempSpikes{i}.total];
-            spikes.amplitudes = cat(2,spikes.amplitudes,tempSpikes{i}.amplitudes);
-            spikes.maxWaveformCh = [spikes.maxWaveformCh tempSpikes{i}.maxWaveformCh];
-            spikes.maxWaveformCh1 = [spikes.maxWaveformCh1 tempSpikes{i}.maxWaveformCh1];
-            if isfield(tempSpikes{i},'phy_maxWaveformCh1')
-                spikes.phy_maxWaveformCh1 = [spikes.phy_maxWaveformCh1 tempSpikes{i}.phy_maxWaveformCh1];
+            fields{1}= fieldnames(spikes);
+            fields{2} = fieldnames(tempSpikes{i});
+            if length(fields{1}) >= length(fields{2})
+               base = 1; comp = 2;
+            else
+                base = 2; comp = 1;
             end
-            spikes.phy_amp = [spikes.phy_amp tempSpikes{i}.phy_amp];
-            spikes.numcells = spikes.numcells+tempSpikes{i}.numcells;
-            tempSpindices2 = tempSpikes{i}.spindices(:,2)+length(spikes.UID);
-            spikes.spindices = [spikes.spindices; [tempSpikes{i}.spindices(:,1) tempSpindices2]];
-            spikes.UID = [spikes.UID tempSpikes{i}.UID+length(spikes.UID)];
-            spikes.shankID = [spikes.shankID tempSpikes{i}.shankID];
-            spikes.rawWaveform = cat(2,spikes.rawWaveform,tempSpikes{i}.rawWaveform);
-            spikes.filtWaveform = cat(2,spikes.filtWaveform,tempSpikes{i}.filtWaveform);
-            spikes.rawWaveform_all = cat(2,spikes.rawWaveform_all,tempSpikes{i}.rawWaveform_all);
-            spikes.filtWaveform_all = cat(2,spikes.filtWaveform_all,tempSpikes{i}.filtWaveform_all);
-            spikes.rawWaveform_std = cat(2,spikes.rawWaveform_std,tempSpikes{i}.rawWaveform_std);
-            spikes.filtWaveform_std = cat(2,spikes.filtWaveform_std,tempSpikes{i}.filtWaveform_std);
-            spikes.timeWaveform = cat(2,spikes.timeWaveform,tempSpikes{i}.timeWaveform);
-            spikes.timeWaveform_all = cat(2,spikes.timeWaveform_all,tempSpikes{i}.timeWaveform_all);
-            spikes.peakVoltage = [spikes.peakVoltage tempSpikes{i}.peakVoltage];
-            spikes.channels_all = cat(2,spikes.channels_all, tempSpikes{i}.channels_all);
-            spikes.peakVoltage_sorted = cat(2,spikes.peakVoltage_sorted,tempSpikes{i}.peakVoltage_sorted);
-            spikes.maxWaveform_all = cat(2,spikes.maxWaveform_all,tempSpikes{i}.maxWaveform_all);
-            spikes.peakVoltage_expFitLengthConstant = [spikes.peakVoltage_expFitLengthConstant tempSpikes{i}.peakVoltage_expFitLengthConstant];
+            prevUID = length(spikes.UID);
+            for j = 1:length(fields{base})
+                currentField = fields{base}{j};
+                if sum(contains(fields{comp},currentField))
+                    %check special cases
+                    if contains(currentField, 'spindices')
+                        tempSpindices2 = tempSpikes{i}.spindices(:,2)+prevUID;
+                        spikes.spindices = [spikes.spindices; [tempSpikes{i}.spindices(:,1) tempSpindices2]];
+                    elseif contains(currentField, 'UID')
+                        spikes.UID = [spikes.UID tempSpikes{i}.UID+prevUID];
+                    elseif contains(currentField, 'basename')
+                        if ~(spikes.basename == tempSpikes{i}.basename)
+                            error('Incompatible basenames');
+                        end
+                    elseif contains(currentField, 'numcells')
+                        spikes.numcells = spikes.numcells+tempSpikes{i}.numcells;
+                    elseif contains(currentField, 'sr')
+                        if ~(spikes.sr == tempSpikes{i}.sr)
+                            error('Incompatible sampling rates');
+                        end
+                    elseif contains(currentField, 'processinginfo')
+                        disp('Processing info assumed to be the same')
+                    elseif iscell(spikes.(currentField))
+                        spikes.(currentField) = cat(2,spikes.(currentField),tempSpikes{i}.(currentField));
+                    else
+                        spikes.(currentField) = [spikes.(currentField) tempSpikes{i}.(currentField)];
+                    end
+                else
+                    disp(['Forced to remove ' currentField]);
+                    if isfield(spikes, currentField)
+                        spikes = rmfield(spikes,currentField);
+                    end
+                end
+            end
         end
     end
     basename = basenameFromBasepath(pwd);
     save(strcat(basename,'.spikes.cellinfo.mat'),'spikes');
-    clear tempSpikes i tempSpindices2
+    clear tempSpikes i j tempSpindices2 prevUID base comp
 else
     spikes = loadSpikes('session',session,'clusteringpath',[f.folder filesep f.name]);
 end

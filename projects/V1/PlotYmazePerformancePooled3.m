c0bced515ff6036030c0cad4fe526eb5ae5dcb65
Heath Robinson <heathlarssonrobinson@gmail.com>
2022-07-11 16:43:52 -0400

    adjusted for dropped 12 and 13 animals

--- projects/V1/PlotYmazePerformancePooled3.m
+++ projects/V1/PlotYmazePerformancePooled3.m
@@ -1,4 +1,4 @@
-animals = {'Unimplanted\m11','Unimplanted\m12','Unimplanted\m13','Unimplanted\m14'}; % Create a cell with all the animals.
+animals = {'Unimplanted\m11','Unimplanted\m14'}; % Create a cell with all the animals.
 fontsize = 15;% Just initiating some parameters.
 % 
 % listOfDays = [34 35 36 37 38 39 40 41 42 43 44 45 46 47 48]; % Specify the days of interest

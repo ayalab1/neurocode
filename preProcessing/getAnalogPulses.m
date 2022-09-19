241ce02f5f4a5a1d35d80aca10c89bb7ab518d21
raltodo <93602613+raltodo@users.noreply.github.com>
2022-09-19 16:46:35 -0400

    add check if signal appears to have pulses, and abort if not
    I added a threshold for how likely it is that the signal contains pulses. This is because if the signal does not actually contain pulses (but only noise), the pulse detection algorithm will take a very long time (wasted).
    
    The idea behind this check is that if real pulses are present in the signal, the signal is bi-modal (stable high value and stable low values). This bimodality can be captured by the "effectiveness metric" (e.g. for Otsu thresholding). Usually noise sessions have a value of <0.08 and sessions with pulses have a value around >0.97, but I set a somewhat arbitrary threshold of 0.3, because I found example sessions in which noise could reach ~0.2, and in a long session with few (real) pulses, it is possible for it to be as low as 0.5.

--- preProcessing/getAnalogPulses.m
+++ preProcessing/getAnalogPulses.m
@@ -166,6 +166,29 @@ for jj = 1 : length(analogCh)
         warning('error in attempting to remove subsession baseline: tell Raly');
     end
 
+    % estimate if there are ANY pulses in the signal:
+    % if there are pulses, we expect good separation between high signal (during pulse) and low signal (outside of pulse)
+    % if there are no pulses, we expect more uniform / gaussian noise signal with poor separation (not bimodal)
+    emThreshold = 0.3; % effectiveness metric goes from 0 (poor separation) to 1 (best separation); the number is an arbitrary threshold that Raly made up (having seen largest noise em-s of ~0.2, and lowest em-s of real pulses around >0.5)
+    try % define "effectiveness metric" for the off-vs-on signal separation (pulse) for each subsession:
+        MergePoints = getStruct(basepath,'MergePoints');
+        for subsession = 1:size(MergePoints.timestamps,1)
+            start = MergePoints.timestamps(subsession,1); stop = MergePoints.timestamps(subsession,2);
+            in = xt>start & xt<stop;
+            dd = double(d(in)); midpoint = mean([min(dd) max(dd)]);
+            em(subsession,1) = 1-(mean(dd>=midpoint)*var(dd(dd>=midpoint),1)+ mean(dd<midpoint)*var(dd(dd<midpoint),1)) / var(dd);
+        end
+    catch % no subsessions detected, use whole session:
+        dd = double(d); midpoint = mean([min(dd) max(dd)]);
+        em = 1-(mean(dd>=midpoint)*var(dd(dd>=midpoint),1)+ mean(dd<midpoint)*var(dd(dd<midpoint),1)) / var(dd);
+        warning('error in attempting to remove subsession baseline: tell Raly');
+    end
+    if max(em)<emThreshold, % if ANY subsession has good pulses, that's enough for this condition to pass
+        warning('Signal didn''t pass Raly''s threshold for detecting pulses. Aborting... Talk to Raly if you think this is a mistake and pulses are actually in the signal.'); 
+        d(:) = 0; % set signal to zero to avoid tedious steps to find pulses
+    end
+
+    
     if any(d<0) % if signal go negative, rectify
         d = d - min(d);
     end

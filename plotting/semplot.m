7a7ddac843d5c73b8bb84053c00dd21fc8ecef0c
raltodo <93602613+raltodo@users.noreply.github.com>
2022-10-13 12:20:16 -0400

    add SEE ALSO

--- plotting/semplot.m
+++ plotting/semplot.m
@@ -5,6 +5,8 @@ function varargout = semplot(x,y,color,smooth)
 %
 % Copyright (C) 2017 by Ralitsa Todorova
 %
+% SEE ALSO shadedErrorBar 
+%
 % This program is free software; you can redistribute it and/or modify
 % it under the terms of the GNU General Public License as published by
 % the Free Software Foundation; either version 3 of the License, or

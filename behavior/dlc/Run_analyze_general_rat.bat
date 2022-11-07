670950f70c256e55a515f73d741b026d783dd18b
Ryan Harvey <ryanh412@gmail.com>
2022-11-07 12:06:06 -0500

    auto dlc for hmc2

---
+++ behavior/dlc/Run_analyze_general_rat.bat
@@ -0,0 +1,4 @@
+@echo on
+call C:\Users\Cornell\anaconda3\Scripts\activate.bat
+call conda activate C:\Users\Cornell\anaconda3\envs\DEEPLABCUT
+python "D:\github\neurocode\behavior\dlc\analyze_general_rat.py"

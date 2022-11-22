3e0863f80bbadb32b9c2dd80c375d553f7f51e85
canliu0414 <49167749+canliu0414@users.noreply.github.com>
2022-11-22 12:39:07 -0500

    Create Run_startdlc_cl.bat

---
+++ behavior/dlc/Run_startdlc_cl.bat
@@ -0,0 +1,5 @@
+@echo on
+call C:\Users\Cornell\anaconda3\Scripts\activate.bat
+call conda activate C:\Users\Cornell\.conda\envs\DEEPLABCUT
+python C:\Users\Cornell\Documents\GitHub\neurocode\behavior\dlc\start_dlc.py
+

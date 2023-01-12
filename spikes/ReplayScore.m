ac86ad2e872a414c5af0106a3df51df38d067cc3
raltodo <93602613+raltodo@users.noreply.github.com>
2023-01-11 21:10:20 -0500

    add wrapper that saves sequence scores in a structure

---
+++ spikes/ReplayScore.m
@@ -0,0 +1,72 @@
+function [sequence,shuffled] = ReplayScore(matrix,varargin)
+
+% ReplayScore
+% This is a structure wrapper which calls "FindReplayScore"
+%
+% USAGE
+%
+%    [r,p,a,b,rShuffled,c,cShuffled,jump,jumpShuffled,maxJump, maxJumpShuffled] = ReplayScore(matrix,threshold,<options>);
+%
+%  INPUT
+%
+%    matrix     probability matrix for a specific event ("estimations" output of ReconstructPosition)       
+%    threshold  distance away from the fitted line (in bins) to count towards the score (see Davidson et al. 2009)
+%    
+%    =========================================================================
+%     Properties    Values
+%    -------------------------------------------------------------------------
+%     'threshold'   considered distance from the line (default = 15 bins)
+%     'nShuffles'   default = 500
+%     'circular'    for circular-linear data (default = 'on')
+%    =========================================================================
+%
+%   OUTPUT
+%
+%     r               replay score of matrix
+%     p               p-value of replay score (based on a column shuffle)
+%     start           start position bin of the fitted line
+%     stop            stop position bin of the fitted line
+%     slope           slope of the fitted line (in bin units)
+%     rShuffled       replay scores of shuffled matrices
+%     stShuffled      st of shuffled matrices
+%     spShuffled      sp of shuffled matrices
+%     wc              weigthed correlation of matrix
+%     wcShuffled       weigthed correlations of shuffled matrices
+%     jump            jump value of matrix  
+%     jumpShuffled    jump values of shuffled matrices
+%     maxJump         max jump value of matrix
+%     maxJumpShuffled max jump values of shuffled matrices
+%
+%  SEE ALSO
+%
+% Copyright (C) 2023 by Ralitsa Todorova
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
+%
+%------------------------------------------------------------------------
+
+[r,p,st,sp,rShuffled,aShuffled,bShuffled,c,cShuffled,jump,jumpShuffled,maxJump,maxJumpShuffled,quadrantScore] = FindReplayScore(matrix,varargin{:});
+
+sequence.quadrantScore = quadrantScore;
+sequence.score = r;
+sequence.p = p;
+sequence.lineStart = st;
+sequence.lineStop = sp;
+sequence.slope = (sp-st)./size(matrix,2);
+sequence.weightedCorrelation = c;
+sequence.jump.mean = jump;
+sequence.jump.max = maxJump;
+shuffled.r = rShuffled;
+shuffled.lineStart = aShuffled;
+shuffled.lineStop = bShuffled;
+shuffled.weightedCorrelation = cShuffled;
+shuffled.jump.mean = jumpShuffled;
+shuffled.jump.max = maxJumpShuffled;
+
+
+
+
+

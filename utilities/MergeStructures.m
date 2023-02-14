b6d5a496af772f8d10dbe92f77ee20a951a63283
raltodo <93602613+raltodo@users.noreply.github.com>
2023-02-13 19:08:33 -0500

    improve MergeStructures to allow nesting structures
    This makes the fix in pipelineReplay obsolete

--- utilities/MergeStructures.m
+++ utilities/MergeStructures.m
@@ -1,10 +1,18 @@
-function merged = MergeStructures(structure1,structure2)
+function merged = MergeStructures(structure1,structure2,dim)
 
 % This only works if the two structures have the same fields and all the
 % sizes are compatible
 
+if ~exist('dim','var')
+    dim = 1;
+end
+
 names = fieldnames(structure2);
-for i=1:length(names)              
-    merged.(names{i})= cat(1,structure1.(names{i}), structure2.(names{i}));
+for i=1:length(names)            
+    if ~isstruct(structure1.(names{i}))
+        merged.(names{i})= cat(dim,structure1.(names{i}), structure2.(names{i}));
+    else % allow nesting
+        merged.(names{i})= MergeStructures(structure1.(names{i}), structure2.(names{i}),dim);
+    end
 end
 

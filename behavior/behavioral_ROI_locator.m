31d3d226affefbd2a1c951aa5ca6e6a80b25842a
Heath Robinson <heathlarssonrobinson@gmail.com>
2023-05-15 15:42:19 -0400

    Update behavioral_ROI_locator.m

--- behavior/behavioral_ROI_locator.m
+++ behavior/behavioral_ROI_locator.m
@@ -23,13 +23,15 @@ function behavioral_ROI_locator(varargin)
     %  'ROI_per_epoch': true or false. If true, will locate ROIs for each epoch. Default is false.
     %  'ROI_per_epoch_number': number of ROIs to locate per epoch. Default is 1.
     %  'ROI_save': true or false. If true, will save the ROIs. Default is false.
-    
+    %  'ROI_force': true or false. If true, will force the ROI locator to run even if the ROIs have
+    %  'ROI_find
     p = inputParser;
     addParameter(p,'ROI_type','circle',@ischar);
     addParameter(p,'ROI_number',1,@isnumeric);
     addParameter(p,'ROI_per_epoch',false,@islogical);
     addParameter(p,'ROI_per_epoch_number',1,@isnumeric);
     addParameter(p,'ROI_save',false,@islogical);
+    addParameter(p,'ROI_force',false,@islogical);
 
     parse(p,varargin{:});
     basepath = p.Results.basepath;
@@ -38,6 +40,7 @@ function behavioral_ROI_locator(varargin)
     ROI_per_epoch = p.Results.ROI_per_epoch;
     ROI_per_epoch_number = p.Results.ROI_per_epoch_number;
     ROI_save = p.Results.ROI_save;
+    ROI_force = p.Results.ROI_force;
 
     if ~iscell(basepaths)
         basepaths = {basepaths};
@@ -47,4 +50,18 @@ function behavioral_ROI_locator(varargin)
     for i = 1:length(basepaths)
         basepath = basepaths{i};
         basename = basenameFromBasepath(basepath);
-        if
+        if exists([basepath, filesep, basename, '.session.mat']) &&...
+            ~ROI_force
+            load([basepath, filesep, basename, '.session.mat']);
+        else disp(['No session file found for ', basename]);
+            continue
+        end
+        if exists([basepath,filesep,basename,'.behavior_roi.mat']) &&...
+            ~ROI_force
+            load([basepath,filesep,basename,'.behavior_roi.mat']);
+        else
+            disp(['No roi file found for ', basename, '. Creating new roi file.']);
+            continue
+        end
+
+        

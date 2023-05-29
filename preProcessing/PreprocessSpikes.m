688472b313cd3fca5eb5475c3a4be05f0858f4df
Heath Robinson <heathlarssonrobinson@gmail.com>
2023-05-29 16:26:35 -0400

    Tagging issue with spikeLabels

--- preProcessing/PreprocessSpikes.m
+++ preProcessing/PreprocessSpikes.m
@@ -51,7 +51,7 @@ multiKilosort = p.Results.multiKilosort;
 showGUI = p.Results.showGUI;
 showCellMet = p.Results.showCellMet;
 prePhy = p.Results.prePhy;
-spikeLabels = p.Results.spikeLabels;
+spikeLabels = p.Results.spikeLabels; %spike labels is causing issues even when good channels are noted - Heathlarsson 05/29/23
 
 %% 1- extract spike times and waveforms for sorted clusters
 cd(basepath);

8dd3618721c7f64fa4f7403a73bf1171f58a108b
Ryan Harvey <ryanh412@gmail.com>
2023-08-21 13:10:53 -0400

    autoformat
    testing autoformat with MBeautify

--- loadData/load_epoch.m
+++ loadData/load_epoch.m
@@ -9,56 +9,56 @@ function epochs = load_epoch(varargin)
 %
 % Ryan Harvey 2023
 
-p=inputParser;
-addParameter(p,'basepath',pwd);
-parse(p,varargin{:});
+p = inputParser;
+addParameter(p, 'basepath', pwd);
+parse(p, varargin{:});
 basepath_ = p.Results.basepath;
 basename = basenameFromBasepath(basepath_);
 
 % load session file
-load(fullfile(basepath_,[basename,'.session.mat']),'session')
+load(fullfile(basepath_, [basename, '.session.mat']), 'session')
 
 % iter over epochs and pull out metadata
 for ep = 1:length(session.epochs)
-    
-    name{ep,1} = session.epochs{ep}.name;
-    startTime(ep,1) = session.epochs{ep}.startTime;
-    stopTime(ep,1) = session.epochs{ep}.stopTime;
-    
-    if isfield(session.epochs{ep},'environment')
-        environment{ep,1} = session.epochs{ep}.environment;
+
+    name{ep, 1} = session.epochs{ep}.name;
+    startTime(ep, 1) = session.epochs{ep}.startTime;
+    stopTime(ep, 1) = session.epochs{ep}.stopTime;
+
+    if isfield(session.epochs{ep}, 'environment')
+        environment{ep, 1} = session.epochs{ep}.environment;
     else
-        environment{ep,1} = [];
+        environment{ep, 1} = [];
     end
-    
-    if isfield(session.epochs{ep},'behavioralParadigm')
-        behavioralParadigm{ep,1} = session.epochs{ep}.behavioralParadigm;
+
+    if isfield(session.epochs{ep}, 'behavioralParadigm')
+        behavioralParadigm{ep, 1} = session.epochs{ep}.behavioralParadigm;
     else
-        behavioralParadigm{ep,1} = [];
+        behavioralParadigm{ep, 1} = [];
     end
-    if isfield(session.epochs{ep},'manipulation')
-        manipulation{ep,1} = session.epochs{ep}.manipulation;
+    if isfield(session.epochs{ep}, 'manipulation')
+        manipulation{ep, 1} = session.epochs{ep}.manipulation;
     else
-        manipulation{ep,1} = {};
+        manipulation{ep, 1} = {};
     end
-    
-    if isfield(session.epochs{ep},'stimuli')
-        stimuli{ep,1} = session.epochs{ep}.stimuli;
+
+    if isfield(session.epochs{ep}, 'stimuli')
+        stimuli{ep, 1} = session.epochs{ep}.stimuli;
     else
-        stimuli{ep,1} = [];
+        stimuli{ep, 1} = [];
     end
-    
-    if isfield(session.epochs{ep},'notes')
-        notes{ep,1} = session.epochs{ep}.notes;
+
+    if isfield(session.epochs{ep}, 'notes')
+        notes{ep, 1} = session.epochs{ep}.notes;
     else
-        notes{ep,1} = [];
+        notes{ep, 1} = [];
     end
-    
-    basepath{ep,1} = basepath_;
+
+    basepath{ep, 1} = basepath_;
 end
 
 % place into table
-epochs = table(name,startTime,stopTime,environment,behavioralParadigm,...
-    manipulation,stimuli,notes,basepath);
+epochs = table(name, startTime, stopTime, environment, behavioralParadigm, ...
+    manipulation, stimuli, notes, basepath);
 
 end

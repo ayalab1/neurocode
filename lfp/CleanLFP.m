0ef01865f74a6acbcc3884e6362cc0eabb505019
Ryan Harvey <ryanh412@gmail.com>
2024-04-28 12:05:05 -0400

    autoformat

--- lfp/CleanLFP.m
+++ lfp/CleanLFP.m
@@ -1,4 +1,4 @@
-function [clean, bad, badIntervals] = CleanLFP(lfp,varargin)
+function [clean, bad, badIntervals] = CleanLFP(lfp, varargin)
 % CleanLFP: removes artefacts from lfp
 %
 % Returns the lfp cleaned from two kinds of artefacts: big artefacts in the
@@ -41,35 +41,34 @@ function [clean, bad, badIntervals] = CleanLFP(lfp,varargin)
 % (at your option) any later version.
 
 % Default values:
-thresholds = [5 10];
-aroundArtefact = [0.5 0.1];
+thresholds = [5, 10];
+aroundArtefact = [0.5, 0.1];
 manual = false;
 
-for i = 1:2:length(varargin),
-    if ~ischar(varargin{i}),
-        error(['Parameter ' num2str(i+2) ' is not a property (type ''help <a href="matlab:help CleanLFP">CleanLFP</a>'' for details).']);
+for i = 1:2:length(varargin)
+    if ~ischar(varargin{i})
+        error(['Parameter ', num2str(i+2), ' is not a property (type ''help <a href="matlab:help CleanLFP">CleanLFP</a>'' for details).']);
     end
-    switch(lower(varargin{i})),
-        case 'thresholds',
+    switch (lower(varargin{i}))
+        case 'thresholds'
             thresholds = varargin{i+1};
-            if ~isvector(thresholds) || length(thresholds) ~= 2,
+            if ~isvector(thresholds) || length(thresholds) ~= 2
                 error('Incorrect value for property ''thresholds'' (type ''help <a href="matlab:help CleanLFP">CleanLFP</a>'' for details).');
             end
-        case 'aroundartefact',
+        case 'aroundartefact'
             aroundArtefact = varargin{i+1};
-            if ~isvector(aroundArtefact) || length(aroundArtefact) ~= 2,
+            if ~isvector(aroundArtefact) || length(aroundArtefact) ~= 2
                 error('Incorrect value for property ''aroundArtefact'' (type ''help <a href="matlab:help CleanLFP">CleanLFP</a>'' for details).');
             end
-        case 'manual',
+        case 'manual'
             manual = varargin{i+1};
-            if ~islogical(manual) || length(manual) ~= 1,
+            if ~islogical(manual) || length(manual) ~= 1
                 error('Incorrect value for property ''manual'' (type ''help <a href="matlab:help CleanLFP">CleanLFP</a>'' for details).');
             end
-        otherwise,
-            error(['Unknown property ''' num2str(varargin{i}) ''' (type ''help <a href="matlab:help CleanLFP">CleanLFP</a>'' for details).']);
+        otherwise
+            error(['Unknown property ''', num2str(varargin{i}), ''' (type ''help <a href="matlab:help CleanLFP">CleanLFP</a>'' for details).']);
     end
 end
-
 
 
 % parameters
@@ -79,47 +78,60 @@ aroundArtefact1 = aroundArtefact(1); % 2, Big and long artefacts
 threshold2 = thresholds(2); % for derivative of z-scored signal
 aroundArtefact2 = aroundArtefact(2); % 0.1 Very fast fluctuations (short time scale)
 
-t = lfp(:,1);
-values = lfp(:,2);
+t = lfp(:, 1);
+values = lfp(:, 2);
 z = zscore(values);
-d = [diff(z);0];
+d = [diff(z); 0];
 bad = false(size(values));
 
 if manual
-    figure; clf; plot(t,z); hold all; plot(xlim,ones(1,2)*threshold1,'r--'); plot(xlim,-ones(1,2)*threshold1,'r--');  ylabel('lfp signal (z-units'); legend('signal','threshold1');
+    figure;
+    clf;
+    plot(t, z);
+    hold all;
+    plot(xlim, ones(1, 2)*threshold1, 'r--');
+    plot(xlim, -ones(1, 2)*threshold1, 'r--');
+    ylabel('lfp signal (z-units');
+    legend('signal', 'threshold1');
     disp('Feel free to change "threshold1" and type "dbcont" to continue.')
     keyboard;
     if false % Optionally, take a look at the threshold for the derivative
-        clf; plot(t,d); hold all; plot(xlim,ones(1,2)*threshold2,'r--'); plot(xlim,-ones(1,2)*threshold2,'r--'); ylabel('lfp derivative (z-units'); legend('derivative','threshold2');
+        clf;
+        plot(t, d);
+        hold all;
+        plot(xlim, ones(1, 2)*threshold2, 'r--');
+        plot(xlim, -ones(1, 2)*threshold2, 'r--');
+        ylabel('lfp derivative (z-units');
+        legend('derivative', 'threshold2');
         disp('Change "threshold2" manually.')
     end
 end
 % Detect large global artefacts (1)
-artefactInterval = t(FindInterval(abs(z)>threshold1));
-if numel(artefactInterval)==2
-    artefactInterval=artefactInterval(:)';
+artefactInterval = t(FindInterval(abs(z) > threshold1));
+if numel(artefactInterval) == 2
+    artefactInterval = artefactInterval(:)';
 end
 if ~isempty(artefactInterval)
-    artefactInterval = ConsolidateIntervals([artefactInterval(:,1)-aroundArtefact1, artefactInterval(:,2)+aroundArtefact1]);
-    bad = InIntervals(t,artefactInterval);
+    artefactInterval = ConsolidateIntervals([artefactInterval(:, 1) - aroundArtefact1, artefactInterval(:, 2) + aroundArtefact1]);
+    bad = InIntervals(t, artefactInterval);
 else
-    artefactInterval = zeros(0,2);
+    artefactInterval = zeros(0, 2);
 end
 
 %Find noise using the derivative of the zscored signal (2)
-noisyInterval = t(FindInterval(abs(d)>threshold2));
-if numel(noisyInterval)==2
-    noisyInterval=noisyInterval(:)';
+noisyInterval = t(FindInterval(abs(d) > threshold2));
+if numel(noisyInterval) == 2
+    noisyInterval = noisyInterval(:)';
 end
 if ~isempty(noisyInterval)
-    noisyInterval = ConsolidateIntervals([noisyInterval(:,1)-aroundArtefact2, noisyInterval(:,2)+aroundArtefact2]);
-    bad = bad | InIntervals(t,noisyInterval);
+    noisyInterval = ConsolidateIntervals([noisyInterval(:, 1) - aroundArtefact2, noisyInterval(:, 2) + aroundArtefact2]);
+    bad = bad | InIntervals(t, noisyInterval);
 else
-    noisyInterval = zeros(0,2);
+    noisyInterval = zeros(0, 2);
 end
 
 % Substitute noisy signal with interpolated signal as if artefact did not exist
-values(bad) = interp1(t(~bad),values(~bad),t(bad,1));
+values(bad) = interp1(t(~bad), values(~bad), t(bad, 1));
 
 badIntervals = ConsolidateIntervals(sortrows([artefactInterval; noisyInterval]));
 ok = ~isnan(values);

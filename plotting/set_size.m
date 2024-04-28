08f46176c97c85f2ccef9dfd3486e3e70aed38b2
Ryan Harvey <ryanh412@gmail.com>
2024-04-28 17:21:26 -0400

    pt to px

--- plotting/set_size.m
+++ plotting/set_size.m
@@ -1,9 +1,9 @@
-function [fig_width_pt, fig_height_pt] = set_size(width, fraction, subplots)
+function [fig_width_px, fig_height_px] = set_size(width, fraction, subplots)
 %     """Set figure dimensions to avoid scaling in LaTeX.
 %     Parameters
 %     ----------
 %     width: float or string
-%             Document width in points, or string of predined document type
+%             Document width in pixels, or string of predined document type
 %     fraction: float, optional
 %             Fraction of the width which you wish the figure to occupy
 %     subplots: array-like, optional
@@ -11,27 +11,27 @@ function [fig_width_pt, fig_height_pt] = set_size(width, fraction, subplots)
 %     Returns
 %     -------
 %     fig_dim: tuple
-%             Dimensions of figure in inches
+%             Dimensions of figure in px
 %     """
 
 
 if strcmp(width, 'thesis')
-    width_pt = 426.79135;
+    width_px = 569.055;
 elseif strcmp(width, 'beamer')
-    width_pt = 307.28987;
+    width_px = 409.720;
 elseif strcmp(width, 'paper')
-    width_pt = 595.276;
+    width_px = 793.701;
 else
-    width_pt = width;
+    width_px = width;
 end
 %   Width of figure (in pts)
-fig_width_pt = width_pt * fraction;
+fig_width_px = width_px * fraction;
 
 %   Golden ratio to set aesthetic figure height
 %   https://disq.us/p/2940ij3
 golden_ratio = (5^.5 - 1) / 2;
 
 %   Figure height
-fig_height_pt = fig_width_pt * golden_ratio * (subplots(1) / subplots(2));
+fig_height_px = fig_width_px * golden_ratio * (subplots(1) / subplots(2));
 
 end

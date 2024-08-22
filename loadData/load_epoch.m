f76b7fba894281c25b2e23acf6c03b56e3254826
Ryan Harvey <ryanh412@gmail.com>
2024-08-22 17:41:18 -0400

    cell to empty string

--- loadData/load_epoch.m
+++ loadData/load_epoch.m
@@ -39,7 +39,7 @@ for ep = 1:length(session.epochs)
     if isfield(session.epochs{ep}, 'manipulation')
         manipulation{ep, 1} = session.epochs{ep}.manipulation;
     else
-        manipulation{ep, 1} = {};
+        manipulation{ep, 1} = '';
     end
 
     if isfield(session.epochs{ep}, 'stimuli')

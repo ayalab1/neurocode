ed6b8130af5ef33860828c1ffd4381866d631655
raltodo <93602613+raltodo@users.noreply.github.com>
2024-08-29 16:16:41 -0400

    function to get a list of basepaths within a directory

---
+++ loadData/getBasepathList.m
@@ -0,0 +1,27 @@
+function basepathList = getBasepathList(parentDirectory)
+
+%Load a list of basepaths within the queried directory which
+% have been previously saved with "updateBasepathList".
+%
+% Copyright (C) 2024 Ralitsa Todorova
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
+
+basepathList = {};
+if iscell(parentDirectory) % Call the function separately for each cell
+    for i=1:length(parentDirectory)
+        output = getBasepathList(parentDirectory{i});
+        if ~isempty(output)
+            basepathList = cat(1,basepathList,output);
+        end
+    end
+else
+    if exist(fullfile(parentDirectory,'basepathList.mat'),'file')
+        load(fullfile(parentDirectory,'basepathList.mat'),'basepathList');
+    else
+        error(['File ' fullfile(parentDirectory,'basepathList.mat') ' not found. Please call updateBasepathList to create it.']);
+    end
+end

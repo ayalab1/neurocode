a522aabcce9262dde4b2bcc4198211599f11a89b
Ryan Harvey <ryanh412@gmail.com>
2024-10-20 13:38:46 -0400

    autoformat and fix warnings

--- lfp/auto_theta_cycles.m
+++ lfp/auto_theta_cycles.m
@@ -38,13 +38,13 @@ function auto_theta_cycles(varargin)
 % (at your option) any later version.
 
 p = inputParser;
-addParameter(p,'basepath',pwd)
-addParameter(p,'passband',[4,12])
-addParameter(p,'maximize_theta_power',true)
-addParameter(p,'run_parallel',false)
-addParameter(p,'overwrite',false)
+addParameter(p, 'basepath', pwd)
+addParameter(p, 'passband', [4, 12])
+addParameter(p, 'maximize_theta_power', true)
+addParameter(p, 'run_parallel', false)
+addParameter(p, 'overwrite', false)
 
-parse(p,varargin{:})
+parse(p, varargin{:})
 basepath = p.Results.basepath;
 passband = p.Results.passband;
 maximize_theta_power = p.Results.maximize_theta_power;
@@ -56,36 +56,36 @@ if ~iscell(basepath)
 end
 
 % iterate over basepaths
-if run_parallel && length(basepath)>1
+if run_parallel && length(basepath) > 1
     parfor i = 1:length(basepath)
-        run(basepath{i},passband,maximize_theta_power,overwrite)
+        run(basepath{i}, passband, maximize_theta_power, overwrite)
     end
 else
     for i = 1:length(basepath)
-        run(basepath{i},passband,maximize_theta_power,overwrite)
+        run(basepath{i}, passband, maximize_theta_power, overwrite)
     end
 end
 end
 
-function run(basepath,passband,maximize_theta_power,overwrite)
+function run(basepath, passband, maximize_theta_power, overwrite)
 disp(basepath)
 basename = basenameFromBasepath(basepath);
 
 % pass if file already exists
-if exist(fullfile(basepath,[basename,'.thetacycles.events.mat']),'file') && ~overwrite
+if exist(fullfile(basepath, [basename, '.thetacycles.events.mat']), 'file') && ~overwrite
     return
 end
 
 % find deep ca1 lfp channel
-[lfp,channel] = get_deep_ca1_lfp(basepath,passband,maximize_theta_power);
+[lfp, channel] = get_deep_ca1_lfp(basepath, passband, maximize_theta_power);
 if isempty(lfp)
     disp('no ca1 lfp')
     return
 end
 
 % find theta cycles, pass cleaned signal in using clean lfp
-[peaktopeak, troughs, amplitude] = FindThetaCycles(...
-    CleanLFP([lfp.timestamps,double(lfp.data)],'thresholds',[8 Inf])...
+[peaktopeak, troughs, amplitude] = FindThetaCycles( ...
+    CleanLFP([lfp.timestamps, double(lfp.data)], 'thresholds', [8, Inf]) ...
     );
 
 % package output
@@ -95,46 +95,46 @@ thetacycles.amplitude = amplitude;
 thetacycles.amplitudeUnits = 'z-units';
 thetacycles.eventID = [];
 thetacycles.eventIDlabels = [];
-thetacycles.center = median(peaktopeak,2);
-thetacycles.duration = peaktopeak(:,2) - peaktopeak(:,1);
+thetacycles.center = median(peaktopeak, 2);
+thetacycles.duration = peaktopeak(:, 2) - peaktopeak(:, 1);
 thetacycles.detectorinfo.method = 'auto_theta_cycles';
 thetacycles.detectorinfo.theta_channel = channel;
 
 % save to basepath
-save(fullfile(basepath,[basename,'.thetacycles.events.mat']),'thetacycles')
+save(fullfile(basepath, [basename, '.thetacycles.events.mat']), 'thetacycles')
 end
 
-function [lfp,channel] = get_deep_ca1_lfp(basepath,passband,maximize_theta_power)
+function [lfp, channel] = get_deep_ca1_lfp(basepath, passband, maximize_theta_power)
 % get_deep_ca1_lfp: locates a deep ca1 channel that maximizes theta power
 
 lfp = [];
-channel=[];
+channel = [];
 
 basename = basenameFromBasepath(basepath);
 
-load(fullfile(basepath,[basename,'.session.mat']))
+load(fullfile(basepath, [basename, '.session.mat']), 'session')
 
-if ~exist(fullfile(basepath,[basename,'.deepSuperficialfromRipple.channelinfo.mat']),'file')
-    classification_DeepSuperficial(session,'basepath',basepath,'sample_ripples',true);
+if ~exist(fullfile(basepath, [basename, '.deepSuperficialfromRipple.channelinfo.mat']), 'file')
+    classification_DeepSuperficial(session, 'basepath', basepath, 'sample_ripples', true);
 end
-load(fullfile(basepath,[basename,'.deepSuperficialfromRipple.channelinfo.mat']))
+load(fullfile(basepath, [basename, '.deepSuperficialfromRipple.channelinfo.mat']), 'deepSuperficialfromRipple')
 
 % find deep ca1 channels to check
 try
-    deep_channels = deepSuperficialfromRipple.channel(contains(deepSuperficialfromRipple.channelClass,'Deep'));
+    deep_channels = deepSuperficialfromRipple.channel(contains(deepSuperficialfromRipple.channelClass, 'Deep'));
 catch % different spelling
-    deep_channels = deepSuperficialfromRipple.channels(contains(deepSuperficialfromRipple.channelClass,'Deep'));
+    deep_channels = deepSuperficialfromRipple.channels(contains(deepSuperficialfromRipple.channelClass, 'Deep'));
 end
 if isempty(deep_channels)
     return
 end
 
 % check for CA1 channels
-deep_channels_temp = identify_good_deep_hpc_channel(session,deep_channels,'CA1');
+deep_channels_temp = identify_good_deep_hpc_channel(session, deep_channels, 'CA1');
 
 % if no CA1, check CA2
 if isempty(deep_channels_temp)
-    deep_channels_temp = identify_good_deep_hpc_channel(session,deep_channels,'CA2');
+    deep_channels_temp = identify_good_deep_hpc_channel(session, deep_channels, 'CA2');
 end
 % if no CA1 or CA2, return
 if isempty(deep_channels_temp)
@@ -144,60 +144,62 @@ else
 end
 
 % load deep channels
-[r,c] = size(deep_channels);
-if r>c
+[r, c] = size(deep_channels);
+if r > c
     deep_channels = deep_channels';
 end
 
 if maximize_theta_power
     % try to load downsampled to same time
     try
-        lfp = getLFP(deep_channels,'basepath',basepath,'downsample',10,...
-            'basename',basename);
+        lfp = getLFP(deep_channels, 'basepath', basepath, 'downsample', 10, ...
+            'basename', basename);
 
         % if sample rate cannot be factored by 10, load entire file
     catch
-        lfp = getLFP(deep_channels,'basepath',basepath,...
-            'basename',basename);
+        lfp = getLFP(deep_channels, 'basepath', basepath, ...
+            'basename', basename);
     end
 
     % get theta power to choose channel
     try
-        pBand = bandpower(single(lfp.data),...
-            lfp.samplingRate,passband);
+        pBand = bandpower(single(lfp.data), ...
+            lfp.samplingRate, passband);
 
-        pTot = bandpower(single(lfp.data),...
-            lfp.samplingRate,...
-            [1,(lfp.samplingRate/2)-1]);
+        pTot = bandpower(single(lfp.data), ...
+            lfp.samplingRate, ...
+            [1, (lfp.samplingRate / 2) - 1]);
     catch
-        for c = 1:size(lfp,2)
-            pBand(c) = bandpower(single(lfp.data(:,c)),...
-                lfp.samplingRate,passband);
+        pBand = zeros(1, size(lfp, 2));
+        pTot = zeros(1, size(lfp, 2));
+        for c = 1:size(lfp, 2)
+            pBand(c) = bandpower(single(lfp.data(:, c)), ...
+                lfp.samplingRate, passband);
 
-            pTot(c) = bandpower(single(lfp.data(:,c)),...
-                lfp.samplingRate,...
-                [1,(lfp.samplingRate/2)-1]);
+            pTot(c) = bandpower(single(lfp.data(:, c)), ...
+                lfp.samplingRate, ...
+                [1, (lfp.samplingRate / 2) - 1]);
         end
     end
     % find max theta power, normalized by wide band
-    [~,c_idx] = max(pBand./pTot);
+    [~, c_idx] = max(pBand./pTot);
 
     % only leave theta channel
-    lfp = getLFP(lfp.channels(:,c_idx),'basepath',basepath,...
-        'basename',basename);
+    lfp = getLFP(lfp.channels(:, c_idx), 'basepath', basepath, ...
+        'basename', basename);
 
 else
-    lfp = getLFP(randsample(deep_channels,1),'basepath',basepath,...
-        'basename',basename);
+    lfp = getLFP(randsample(deep_channels, 1), 'basepath', basepath, ...
+        'basename', basename);
 end
 channel = lfp.channels;
 end
 
-function deep_channels = identify_good_deep_hpc_channel(session,deep_channels,region)
+function deep_channels = identify_good_deep_hpc_channel(session, deep_channels, region)
 
 % search for ca1 channels
 brainRegions = fields(session.brainRegions);
-ca1_layers = brainRegions(contains(brainRegions,region));
+ca1_layers = brainRegions(contains(brainRegions, region));
 
 if isempty(ca1_layers)
     deep_channels = [];
@@ -205,16 +207,16 @@ if isempty(ca1_layers)
 end
 ca1_channels = [];
 for region = ca1_layers'
-    ca1_channels = [ca1_channels;session.brainRegions.(region{1}).channels'];
+    ca1_channels = [ca1_channels; session.brainRegions.(region{1}).channels'];
 end
 ca1_channels = unique(ca1_channels);
 
 % find channels that are deep and ca1
-deep_channels = deep_channels(ismember(deep_channels,ca1_channels))';
+deep_channels = deep_channels(ismember(deep_channels, ca1_channels))';
 
 % kick out bad channels
 try
-    deep_channels = deep_channels(~ismember(deep_channels,session.channelTags.Bad.channels));
+    deep_channels = deep_channels(~ismember(deep_channels, session.channelTags.Bad.channels));
 catch
 end
 

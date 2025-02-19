181e0fb5abb93a93fc97c7b38fe158e6148a8f76
Ryan Harvey <ryanh412@gmail.com>
2025-02-19 11:24:04 -0500

    update formatting

--- plotting/PlotIntervals.m
+++ plotting/PlotIntervals.m
@@ -1,4 +1,4 @@
-function varargout = PlotIntervals(intervals,varargin)
+function varargout = PlotIntervals(intervals, varargin)
 
 %PlotIntervals - Plot vertical bars or rectangles to show interval limits.
 %
@@ -25,7 +25,7 @@ function varargout = PlotIntervals(intervals,varargin)
 %    =========================================================================
 %
 
-% Copyright (C) 2008-2013 by Gabrielle Girardeau & Michaël Zugaro, 
+% Copyright (C) 2008-2013 by Gabrielle Girardeau & Michaël Zugaro,
 % (C) 2023 by Ralitsa Todorova (graphics optimization for large numbers of intervals)
 %
 % This program is free software; you can redistribute it and/or modify
@@ -34,101 +34,100 @@ function varargout = PlotIntervals(intervals,varargin)
 % (at your option) any later version.
 
 % Default values
-color = [0.9 0.9 0.9];
+color = [0.9, 0.9, 0.9];
 alphaValue = 0.5;
 style = 'rectangles';
 direction = 'v';
 yLim = ylim;
 
 if nargin < 1
-  error('Incorrect number of parameters (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).');
+    error('Incorrect number of parameters (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).');
 end
-if size(intervals,2) ~= 2
-  error('Incorrect list of intervals (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).');
+if size(intervals, 2) ~= 2
+    error('Incorrect list of intervals (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).');
 end
 
 % Backward compatibility: previous versions used the syntax PlotIntervals(intervals,style,direction)
 parsed = false;
-if (nargin == 2 || nargin == 3) && isastring(lower(varargin{1}),'rectangles','bars')
-	style = lower(varargin{1});
-	parsed = true;
+if (nargin == 2 || nargin == 3) && isastring(lower(varargin{1}), 'rectangles', 'bars')
+    style = lower(varargin{1});
+    parsed = true;
 end
-if nargin == 3 && isastring(lower(varargin{2}),'h','v')
-	direction = lower(varargin{2});
-	parsed = true;
+if nargin == 3 && isastring(lower(varargin{2}), 'h', 'v')
+    direction = lower(varargin{2});
+    parsed = true;
 end
 
 % Parse parameter list
 if ~parsed
-	for i = 1:2:length(varargin)
-		if ~ischar(varargin{i})
-			error(['Parameter ' num2str(i+1) ' is not a property (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).']);
-		end
-		switch(lower(varargin{i}))
-			case 'style'
-				style = lower(varargin{i+1});
-				if ~isastring(style,'bars','rectangles')
-					error('Incorrect value for property ''style'' (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).');
-				end
-			case 'direction'
-				direction = lower(varargin{i+1});
-				if ~isastring(direction,'h','v')
-					error('Incorrect value for property ''direction'' (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).');
-				end
-			case 'color'
-				color = lower(varargin{i+1});
-				if ~isastring(color,'r','g','b','c','m','y','k','w') && ~isdvector(color,'#3','>=0','<=1')
-					error('Incorrect value for property ''direction'' (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).');
-				end
-			case 'alpha'
+    for i = 1:2:length(varargin)
+        if ~ischar(varargin{i})
+            error(['Parameter ', num2str(i+1), ' is not a property (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).']);
+        end
+        switch (lower(varargin{i}))
+            case 'style'
+                style = lower(varargin{i+1});
+                if ~isastring(style, 'bars', 'rectangles')
+                    error('Incorrect value for property ''style'' (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).');
+                end
+            case 'direction'
+                direction = lower(varargin{i+1});
+                if ~isastring(direction, 'h', 'v')
+                    error('Incorrect value for property ''direction'' (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).');
+                end
+            case 'color'
+                color = lower(varargin{i+1});
+                if ~isastring(color, 'r', 'g', 'b', 'c', 'm', 'y', 'k', 'w') && ~isdvector(color, '#3', '>=0', '<=1')
+                    error('Incorrect value for property ''direction'' (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).');
+                end
+            case 'alpha'
                 alphaValue = varargin{i+1};
-                if ~isdscalar(alphaValue,'>=0','<=1')
+                if ~isdscalar(alphaValue, '>=0', '<=1')
                     error('Incorrect value for property ''alpha'' (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).');
                 end
             case 'ylim'
                 yLim = varargin{i+1};
-                if ~isdvector(yLim,'<')
+                if ~isdvector(yLim, '<')
                     error('Incorrect value for property ''yLim'' (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).');
                 end
             otherwise
-				error(['Unknown property ''' num2str(varargin{i}) ''' (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).']);
-		end
-	end
+                error(['Unknown property ''', num2str(varargin{i}), ''' (type ''help <a href="matlab:help PlotIntervals">PlotIntervals</a>'' for details).']);
+        end
+    end
 end
 
 hold on;
 xLim = xlim;
-if strcmp(style,'bars')
-	for i = 1:size(intervals,1)
-		if strcmp(direction,'v')
-			plot([intervals(i,1) intervals(i,1)],yLim,'Color',[0 0.75 0]);
-			plot([intervals(i,2) intervals(i,2)],yLim,'Color',[0.9 0 0]);
-		else
-			plot(xLim,[intervals(i,1) intervals(i,1)],'Color',[0 0.75 0]);
-			plot(xLim,[intervals(i,2) intervals(i,2)],'Color',[0.9 0 0]);
-		end
-	end
-else
-	for i=1:size(intervals,1)
-		if strcmp(direction,'v')
-			dx = intervals(i,2)-intervals(i,1);
-			dy = yLim(2)-yLim(1);
-			rec(i) = patch(intervals(i,1)+[0 0 dx dx],yLim(1)+[0 dy dy 0],color,'LineStyle','none');
-			alpha(rec(i),alphaValue);
-		else
-			dx = xLim(2)-xLim(1);
-			dy = intervals(i,2)-intervals(i,1);
-			rec(i) = patch(xLim(1)+[0 0 dx dx],intervals(i,1)+[0 dy dy 0],color,'LineStyle','none');
-			alpha(rec(i),alphaValue);
-			alpha(rec(i),alphaValue);
+if strcmp(style, 'bars')
+    for i = 1:size(intervals, 1)
+        if strcmp(direction, 'v')
+            plot([intervals(i, 1), intervals(i, 1)], yLim, 'Color', [0, 0.75, 0]);
+            plot([intervals(i, 2), intervals(i, 2)], yLim, 'Color', [0.9, 0, 0]);
+        else
+            plot(xLim, [intervals(i, 1), intervals(i, 1)], 'Color', [0, 0.75, 0]);
+            plot(xLim, [intervals(i, 2), intervals(i, 2)], 'Color', [0.9, 0, 0]);
         end
     end
-    if exist('rec','var') % if something was actually plotted
-        uistack(rec,'bottom'); % show it
+else
+    for i = 1:size(intervals, 1)
+        if strcmp(direction, 'v')
+            dx = intervals(i, 2) - intervals(i, 1);
+            dy = yLim(2) - yLim(1);
+            rec(i) = patch(intervals(i, 1)+[0, 0, dx, dx], yLim(1)+[0, dy, dy, 0], color, 'LineStyle', 'none');
+            alpha(rec(i), alphaValue);
+        else
+            dx = xLim(2) - xLim(1);
+            dy = intervals(i, 2) - intervals(i, 1);
+            rec(i) = patch(xLim(1)+[0, 0, dx, dx], intervals(i, 1)+[0, dy, dy, 0], color, 'LineStyle', 'none');
+            alpha(rec(i), alphaValue);
+            alpha(rec(i), alphaValue);
+        end
+    end
+    if exist('rec', 'var') % if something was actually plotted
+        uistack(rec, 'bottom'); % show it
     end
 end
 
-if nargout>0
+if nargout > 0
     varargout{1} = rec;
 end
-

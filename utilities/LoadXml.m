585f7691e0afb90e24856f891678522481420028
Ryan Harvey <ryanh412@gmail.com>
2025-08-16 12:31:32 -0400

    fix formatting

--- utilities/LoadXml.m
+++ utilities/LoadXml.m
@@ -10,13 +10,13 @@
 % this script is written for xml version 1.1 .. older version doesn't work.
 % additions are welcome
 
-function [xml, rxml] = LoadXml(fbasename,varargin)
+function [xml, rxml] = LoadXml(fbasename, varargin)
 
 xml = struct;
 
-xmli = strfind(fbasename,'.xml');
+xmli = strfind(fbasename, '.xml');
 if isempty(xmli)
-   fbasename = [fbasename '.xml'];
+    fbasename = [fbasename, '.xml'];
 end
 rxml = xmltools(fbasename);
 try
@@ -27,42 +27,36 @@ end
 % from this level all children are the different parameters fields
 xml.FileName = fbasename;
 
-for i=1:length(rxml.child)
-
+for i = 1:length(rxml.child)
     switch lower(rxml.child(i).tag)
-        
         case 'generalinfo'
             xml.Date = rxml.child(i).child(1).value; % date of xml file creation?
-
         case 'acquisitionsystem'
             xml.nBits = str2num(rxml.child(i).child(1).value); % number of bits of the file
             xml.nChannels = str2num(rxml.child(i).child(2).value);
             xml.SampleRate = str2num(rxml.child(i).child(3).value);
-            xml.SampleTime = 1e6/xml.SampleRate; %to make backwards compatible
+            xml.SampleTime = 1e6 / xml.SampleRate; %to make backwards compatible
             xml.VoltageRange = str2num(rxml.child(i).child(4).value);
             xml.Amplification = str2num(rxml.child(i).child(5).value);
-            xml.Offset =  str2num(rxml.child(i).child(6).value);
-            
+            xml.Offset = str2num(rxml.child(i).child(6).value);
         case 'fieldpotentials'
             xml.lfpSampleRate = str2num(rxml.child(i).child.value);
-            
         case 'anatomicaldescription'
             tmp = rxml.child(i).child.child;
-            for grpI =1:length(tmp)
-                for chI=1:length(tmp(grpI).child)
+            for grpI = 1:length(tmp)
+                for chI = 1:length(tmp(grpI).child)
                     xml.AnatGrps(grpI).Channels(chI) = str2num(tmp(grpI).child(chI).value);
                     xml.AnatGrps(grpI).Skip(chI) = str2num(tmp(grpI).child(chI).attribs.value);
                 end
             end
-            
         case 'spikedetection'
             if ~isempty(rxml.child(i).child)
-                tmp =rxml.child(i).child.child;
-                for grpI =1:length(tmp)
-                    for chI=1:length(tmp(grpI).child(1).child)
+                tmp = rxml.child(i).child.child;
+                for grpI = 1:length(tmp)
+                    for chI = 1:length(tmp(grpI).child(1).child)
                         xml.SpkGrps(grpI).Channels(chI) = str2num(tmp(grpI).child(1).child(chI).value);
                     end
-                    if length(tmp(grpI).child)>1
+                    if length(tmp(grpI).child) > 1
                         xml.SpkGrps(grpI).nSamples = str2num(tmp(grpI).child(2).value);
                         xml.SpkGrps(grpI).PeakSample = str2num(tmp(grpI).child(3).value);
                         xml.SpkGrps(grpI).nFeatures = str2num(tmp(grpI).child(4).value);
@@ -74,14 +68,12 @@ for i=1:length(rxml.child)
             else
                 xml.nElecGps = 0;
             end
-
-
         case 'programs'
             tmp = rxml.child(i).child;
-            for i=1:length(tmp)
-                if strcmp(tmp(i).child(1).value,'process_mhipass')
-                    for j=1:length(tmp(i).child(2).child )
-                        if strcmp(tmp(i).child(2).child(j).child(1).value,'frequency')
+            for i = 1:length(tmp)
+                if strcmp(tmp(i).child(1).value, 'process_mhipass')
+                    for j = 1:length(tmp(i).child(2).child)
+                        if strcmp(tmp(i).child(2).child(j).child(1).value, 'frequency')
                             xml.HiPassFreq = str2num(tmp(i).child(2).child(j).child(2).value);
                             break
                         end
@@ -89,8 +81,6 @@ for i=1:length(rxml.child)
                 end
             end
     end
-
-
 end
 
 
